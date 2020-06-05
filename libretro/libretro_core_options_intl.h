343613692b4099c9eed34efeb8e5d940e8545b83
crazyq <crazyq@gmail.com>
2020-06-05 22:01:23 +0800

    汉化设置菜单

--- libretro/libretro_core_options_intl.h
+++ libretro/libretro_core_options_intl.h
@@ -60,6 +60,195 @@ extern "C" {
 /* RETRO_LANGUAGE_CHINESE_TRADITIONAL */
 
 /* RETRO_LANGUAGE_CHINESE_SIMPLIFIED */
+struct retro_core_option_definition option_defs_chs[] = {
+   {
+      "nestopia_blargg_ntsc_filter",
+      "Blargg NTSC滤镜",
+      "启用Blargg NTSC滤镜。",
+      {
+         { "disabled",   NULL },
+         { "composite",  "复合视频" },
+         { "svideo",     "S-Video" },
+         { "rgb",        "RGB SCART" },
+         { "monochrome", "黑白" },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "nestopia_palette",
+      "调色板",
+      "选择要使用的调色板。",
+      {
+         { "cxa2025as",            "CXA2025AS" },
+         { "consumer",             "Consumer" },
+         { "canonical",            "Canonical" },
+         { "alternative",          "Alternative" },
+         { "rgb",                  "RGB" },
+         { "pal",                  "PAL" },
+         { "composite-direct-fbx", "Composite Direct FBx" },
+         { "pvm-style-d93-fbx",    "PVM-style D93 FBx" },
+         { "ntsc-hardware-fbx",    "NTSC hardware FBx" },
+         { "nes-classic-fbx-fs",   "NES Classic FBx FS" },
+         { "raw",                  "Raw" },
+         { "custom",               "Custom" },
+         { NULL, NULL },
+      },
+      "cxa2025as" /* TODO/FIXME - is this correct ? */
+   },
+   {
+      "nestopia_nospritelimit",
+      "移除角色限制",
+      "移除每扫描线8个角色的硬件限制。",
+      {
+         { "disabled", NULL },
+         { "enabled",  NULL },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "nestopia_overclock",
+      "CPU速度（超频）",
+      "对模拟的CPU进行超频。",
+      {
+         { "1x", NULL },
+         { "2x", NULL },
+         { NULL, NULL },
+      },
+      "1x"
+   },
+   {
+      "nestopia_select_adapter",
+      "4打适配器",
+      "手动选择4打适配器。\n"
+	  "有些游戏无法通过NstDatabase.xml数据库正确识别适配器，此项可以帮助解决此问题。",
+      {
+         { "auto",    "自动" },
+         { "ntsc",    "NTSC" },
+         { "famicom", "Famicom" },
+         { NULL, NULL },
+      },
+      "auto"
+   },
+   {
+      "nestopia_fds_auto_insert",
+      "磁盘自动插入",
+      "复位时自动插入第一张磁盘。",
+      {
+         { "disabled", NULL },
+         { "enabled",  NULL },
+         { NULL, NULL },
+      },
+      "enabled"
+   },
+   {
+      "nestopia_overscan_v",
+      "剪切过扫描（垂直）",
+      "剪切垂直方向的应该被标准电视的边框隐藏的过扫描区域。",
+      {
+         { "enabled",  NULL },
+         { "disabled", NULL },
+         { NULL, NULL },
+      },
+      "enabled"
+   },
+   {
+      "nestopia_overscan_h",
+      "剪切过扫描（水平）",
+      "剪切水平方向的应该被标准电视的边框隐藏的过扫描区域。",
+      {
+         { "enabled",  NULL },
+         { "disabled", NULL },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "nestopia_aspect",
+      "输出宽高比",
+      "选择输出宽高比。RetroArch的宽高比必须设为'内核提供'。\n"
+	  "'自动'会使用NstDatabase.xml数据库文件自动检测宽高比。\n"
+	  "如果没有数据库文件，'自动'缺省为NTSC。",
+      {
+         { "auto", "自动" },
+         { "ntsc", "NTSC" },
+         { "pal",  "PAL" },
+         { "4:3",  "4:3" },
+         { NULL, NULL },
+      },
+      "auto"
+   },
+   {
+      "nestopia_genie_distortion",
+      "金手指卡声音变形",
+      "金手指设备会不经意的导致声音变形。\n"
+	  "启用此项可以模拟这种变形，添加到游戏声音中去。",
+      {
+         { "disabled", NULL },
+         { "enabled",  NULL },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "nestopia_favored_system",
+      "系统区域",
+      "选择系统区域。\n"
+	  "'自动'会使用NstDatabase.xml数据库自动检测区域。\n"
+	  "如果没有数据库文件，'自动'缺省为NTSC。",
+      {
+         { "auto",    "自动" },
+         { "ntsc",    "NTSC" },
+         { "pal",     "PAL" },
+         { "famicom", "Famicom" },
+         { "dendy",   "Dendy" },
+         { NULL, NULL },
+      },
+      "auto"
+   },
+   {
+      "nestopia_ram_power_state",
+      "内存启动状态",
+      "",
+      {
+         { "0x00",   NULL },
+         { "0xFF",   NULL },
+         { "random", "随机" },
+         { NULL, NULL },
+      },
+      "0x00"
+   },
+   {
+      "nestopia_button_shift",
+      "顺时针移动按键",
+      "顺时针旋转A/B/X/Y按键。", /* is this right? @gadsby */
+      {
+         { "disabled", NULL },
+         { "enabled",  NULL },
+         { NULL, NULL },
+      },
+      "disabled"
+   },
+   {
+      "nestopia_turbo_pulse",
+      "连发键速度",
+      "设置连发键A和B的连发速度。",
+      {
+         { "2", NULL },
+         { "3", NULL },
+         { "4", NULL },
+         { "5", NULL },
+         { "6", NULL },
+         { "7", NULL },
+         { "8", NULL },
+         { "9", NULL },
+         { NULL, NULL },
+      },
+      "2"
+   },
+   { NULL, NULL, NULL, {{0}}, NULL },
+};
 
 /* RETRO_LANGUAGE_ESPERANTO */
 
